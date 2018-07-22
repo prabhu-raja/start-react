bc4eaa91b1854e73ad15bd9b2ba542b4da4919a9
prabhu-raja <prabhuraja@gmail.com>
2018-07-22 11:05:16 -0400

    created input component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ZipForm from './ZipForm';
 
 class App extends React.Component {
     render() {
         return <div className="app">
             <p>Rocks🇨🇦</p>
+            <ZipForm />
         </div>
     }
 }
